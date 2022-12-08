c6bc071a4b5b41ce921e1af83cb170d55bc1086b
gurzu <mihail.gurzu@gmail.com>
2022-12-08 12:30:41 +0100

    First commit

---
+++ myproject/src/main/java/com/examples/myproject/App.java
@@ -0,0 +1,17 @@
+package com.examples.myproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	public String sayHello() {
+		return "Hello";
+	}
+//    public static void main( String[] args )
+//    {
+//        System.out.println( "Hello World!" );
+//    }
+}
